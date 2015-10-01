d55590e4636a881305e805b5f8854b26666ed404
Sarah Besozzi <sarah.besozzi@gmail.com>
2015-09-30 20:29:36 -0400

    Finished 5 0f 6

--- main.js
+++ main.js
@@ -23,22 +23,27 @@
 // Q1
 // Write a function called `sum` that takes two
 // parameters and returns the sum of those 2 numbers.
-
+function sum(num1, num2) {
+    return num1 + num2;
+}
 
 
 
 // Q2
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
-
+function avg(num1, num2, num3) {
+  return (num1 + num2 + num3) / 3; 
+}
 
 
 
 // Q3
 // Write a function called `getLength` that takes one
 // parameter (a string) and returns the length
-
-
+function getLength(str) {
+  return str.length;
+}
 
 
 // Q4
@@ -46,8 +51,15 @@
 // two parameters and returns `true` if the second
 // parameter is greater than the first. Otherwise
 // the function should return `false`.
-
-
+function greaterThan(par1, par2) {
+  
+  if (par2 > par1) {
+   true;
+  } 
+  else {
+    false;
+  }
+};
 
 
 // Q5
@@ -55,8 +67,11 @@
 // single parameter and returns a string that
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
+function greet(name) {
 
+  return "Hello, " + name + "!";
 
+};
 
 
 // Q6
@@ -69,6 +84,18 @@
 // strings. For example:
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
+
+// 1. write a function
+// 2. has 4 parameters words
+// 3. put words into sentence
+// 4. return sentence with words
+
+function madLib(sentence) {
+
+    sentence = "word1" + "word2" + "word3" + "word4";
+  return sentence;
+
+};
 
 
 
